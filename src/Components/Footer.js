bb3f9c0c344725e37fc4910d9227fdff63afef22
gsd-init <gsd.init@gmail.com>
2020-10-27 19:00:13 -0700

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+  
+  render() {
+
+    if(this.props.data){
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+              <div>             
+                <div>
+                  {networks}
+                </div>
+              </div>
+              
+           </ul>
+           
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
+    );
+  }
+}
+
+export default Footer;
