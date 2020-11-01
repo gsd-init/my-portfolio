286b2ee4c378346bfe48dc35d6a74c646db0e1f3
gsd-init <gsd.init@gmail.com>
2020-11-01 00:05:43 -0700

    form fixed submit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -42,34 +42,32 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm"> 
-               <input type="hidden" name="form-name" value="contactForm" />
+               <form action="" method="POST" id="contactForm" name="contactForm" data-netlify="true"> 
 					<fieldset>
                   <div>
-						   <label htmlFor="contactName">Name Teste <span className="required">*</span></label>
-						   <input value="contactForm" type="text" defaultValue="" size="35" id="contactName" name="contactName" placeholder="test" onChange={this.handleChange}/>
+						   <label htmlFor="contactName">Name <span className="required">*</span></label>
+						   <input type="text" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value="contactForm" type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+						   <input type="email" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input value="contactForm" type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+						   <input type="text" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea value="contactForm" cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
                   </div>
+
+                  <div data-netlify-recaptcha="true"></div>
 
                   <div className="btn-submit">
                      <button type="submit" className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
                   </div>
 					</fieldset>
 				   </form>
